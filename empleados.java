49d0db5b44b80cca86cc47bb846e88677eb5ff8e
VictorLag <153596770+VictorLag@users.noreply.github.com>
2024-05-14 08:14:47 +0200

    Update empleados.java

--- empleados.java
+++ empleados.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class empleados {
 
@@ -20,6 +21,70 @@ public class empleados {
         NifEmp = nifEmp;
     }
 
+    public int getCodEmp() {
+        return CodEmp;
+    }
+
+    public void setCodEmp(int codEmp) {
+        CodEmp = codEmp;
+    }
+
+    public String getCodDep() {
+        return CodDep;
+    }
+
+    public void setCodDep(String codDep) {
+        CodDep = codDep;
+    }
+
+    public String getExTelEmp() {
+        return ExTelEmp;
+    }
+
+    public void setExTelEmp(String exTelEmp) {
+        ExTelEmp = exTelEmp;
+    }
+
+    public LocalDate getFecInEmp() {
+        return FecInEmp;
+    }
+
+    public void setFecInEmp(LocalDate fecInEmp) {
+        FecInEmp = fecInEmp;
+    }
+
+    public LocalDate getFecNaEmp() {
+        return FecNaEmp;
+    }
+
+    public void setFecNaEmp(LocalDate fecNaEmp) {
+        FecNaEmp = fecNaEmp;
+    }
+
+    public String getNomEmp() {
+        return NomEmp;
+    }
+
+    public void setNomEmp(String nomEmp) {
+        NomEmp = nomEmp;
+    }
+
+    public int getNumHi() {
+        return NumHi;
+    }
+
+    public void setNumHi(int numHi) {
+        NumHi = numHi;
+    }
+
+    public double getSalEmp() {
+        return SalEmp;
+    }
+
+    public void setSalEmp(double salEmp) {
+        SalEmp = salEmp;
+    }
+
     public empleados(int codEmp, String codDep, String exTelEmp, LocalDate fecInEmp , LocalDate fecNaEmp, String nifEmp, String nomEmp, int numHi, double SalEmp ) {
 
         this.CodEmp = codEmp;
@@ -34,8 +99,17 @@ public class empleados {
 
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        empleados empleado = (empleados) o;
+        return CodEmp == empleado.CodEmp && NumHi == empleado.NumHi && Double.compare(SalEmp, empleado.SalEmp) == 0 && Objects.equals(CodDep, empleado.CodDep) && Objects.equals(ExTelEmp, empleado.ExTelEmp) && Objects.equals(FecInEmp, empleado.FecInEmp) && Objects.equals(FecNaEmp, empleado.FecNaEmp) && Objects.equals(NifEmp, empleado.NifEmp) && Objects.equals(NomEmp, empleado.NomEmp);
+    }
 
-
-
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(CodEmp, CodDep, ExTelEmp, FecInEmp, FecNaEmp, NifEmp, NomEmp, NumHi, SalEmp);
+    }
+    
 }
